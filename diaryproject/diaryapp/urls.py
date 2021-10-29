7421c9b83844044d618d820122364d896929b3e2
kino <info@kinoshitashota.com>
2021-10-29 15:47:03 +0900

    heroku 1st deploy

--- diaryproject/diaryapp/urls.py
+++ diaryproject/diaryapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import DiaryList
+from .views import DiaryList, DiaryCreate, DiaryDetail
+
 
 urlpatterns = [
-  path('', DiaryList.as_view()),
-
+  path('', DiaryList.as_view(), name='list'),
+  path('create/', DiaryCreate.as_view(), name='create'),
+  path('detail/<int:pk>', DiaryDetail.as_view(), name='detail'), # int=integer, pk=primary key
+  
 ]
--- diaryproject/diaryapp/views.py
+++ diaryproject/diaryapp/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import DiaryModel
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy
 
 class DiaryList(ListView):
   template_name = 'diaryapp/list.html'
+  model = DiaryModel
+
+class DiaryCreate(CreateView):
+  template_name = 'diaryapp/create.html'
+  model = DiaryModel
+  fields = ('date', 'title', 'text')
+  success_url = reverse_lazy('list')
+
+class DiaryDetail(DetailView):
+  template_name = 'diaryapp/detail.html'
   model = DiaryModel
